3d8139ee124022dfd937566e3635a6cedd286cb4
Joaquintinsin <phoenix_css@hotmail.com>
2023-10-15 13:30:37 -0300

    General update: lab/ == Laboratorio/. Txts

--- lab/teo_08/arithmetic_2019.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARITHMETIC_2019_H // #ifndef se usa para definir el identificador (en este caso ARITHMETIC_H)
-
-// seccion de Interfaz (declaraciones publicas)
-// a continuacion se declaran los perfiles de las funtion que podrán ser utilizadas desde los programas que incluyan a la biblioteca arithmetic
-int is_prime( int n );
-int division( int dividend, int divisor, int * quotient );
-int factorial( int n );
-
-#endif // fin del identificador e interfaz
